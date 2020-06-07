71bb56ea34afb6945762e843fb4ba707328ca770
samanthavictoria <sasavicsouza@gmail.com>
2020-06-08 08:17:31 +0900

    falta css

--- www/components/verificar.js
+++ www/components/verificar.js
@@ -2,4 +2,16 @@
 function botao( let ){
   var sv = document.palavras.visor.value;
   document.palavras.visor.value = sv + let;
+  navigator.notification.beep(1);
 }
+
+$(document).on("click","#verificar", function(){
+  var palavra = $("#visor").val();
+     
+     if(palavra == "abacaxi"){
+        navigator.notification.alert("Resposta correta, parabéns!",null,"Aviso","ok");
+     }
+     else{
+      navigator.vibrate(1000);
+     }
+});
